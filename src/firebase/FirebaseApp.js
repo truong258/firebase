df9d64d7edff61c896f1040ebba583acd8382952
NhatTruong <truongmai750@gmail.com>
2023-11-15 16:57:03 +0700

    update realtime

--- src/firebase/FirebaseApp.js
+++ src/firebase/FirebaseApp.js
@@ -1,4 +1,12 @@
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  onSnapshotsInSync,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "./firebaseconfig";
 
@@ -6,23 +14,37 @@ const FirebaseApp = () => {
   const colRef = collection(db, "posts");
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
+  const [postId, setPostId] = useState("");
+  const [posts, setPosts] = useState([]);
   console.log(colRef);
   useEffect(() => {
-    getDocs(colRef)
-      .then((snapshot) => {
-        //   console.log("snapshot", snapshot);
-        let posts = [];
-        snapshot.docs.forEach((doc) => {
-          posts.push({
-            id: doc.id,
-            ...doc.data(),
-          });
+    // getDocs(colRef)
+    //   .then((snapshot) => {
+    //     //   console.log("snapshot", snapshot);
+    //     let posts = [];
+    //     snapshot.docs.forEach((doc) => {
+    //       posts.push({
+    //         id: doc.id,
+    //         ...doc.data(),
+    //       });
+    //     });
+    //     setPosts(posts);
+    //     console.log("post", posts);
+    //   })
+    //   .catch((error) => {
+    //     console.log("error", error);
+    //   });
+    //2. update real time
+    onSnapshot(colRef, (snapshot) => {
+      let posts = [];
+      snapshot.docs.forEach((doc) => {
+        posts.push({
+          id: doc.id,
+          ...doc.data(),
         });
-        console.log("post", posts);
-      })
-      .catch((error) => {
-        console.log("error", error);
       });
+      setPosts(posts);
+    });
   }, []);
 
   const handelAddPost = (e) => {
@@ -39,9 +61,16 @@ const FirebaseApp = () => {
       });
   };
 
+  const handelRemovePost = async (e) => {
+    e.preventDefault();
+    const colRefDelete = doc(db, "posts", postId);
+    await deleteDoc(colRefDelete);
+    console.log("sucess");
+  };
+
   return (
     <div className="p-10">
-      <div className="w-full max-w-[500px] mx-auto  bg-white shadow-lg p-5 ">
+      <div className="w-full max-w-[500px] mx-auto  bg-white shadow-lg p-5 mb-10">
         <form onSubmit={handelAddPost}>
           <input
             type="text"
@@ -64,6 +93,34 @@ const FirebaseApp = () => {
             Add post
           </button>
         </form>
+      </div>
+      <div className="w-full max-w-[500px] mx-auto  bg-white shadow-lg p-5 mb-10">
+        <form onSubmit={handelRemovePost}>
+          <input
+            type="text"
+            className="p-3 rounded border border-gray-200 w-full mb-5 outline-none focus:border-blue-500"
+            placeholder="Enter id"
+            name="postId"
+            onChange={(e) => setPostId(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="p-3 bg-red-500 text-white text-sm font-medium w-full rounded-lg"
+          >
+            Remove post
+          </button>
+        </form>
+      </div>
+      <div className="w-full max-w-[500px] mx-auto  bg-white shadow-lg p-5 mb-10">
+        {posts.length > 0 &&
+          posts.map((item) => (
+            <div key={item.title}>
+              <div>
+                {item.title}-{item.author}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
